6aa9e9fb8c9630ca79122243333cd9fc3575139b
ramanandhaaja <thisisnandha@gmail.com>
2024-10-23 10:01:33 +0700

    update path

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(except: [
+            '/images','/images/*', '/api/*', 'api/*'
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
