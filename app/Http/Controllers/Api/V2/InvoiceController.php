feb5759497270de28ad4d10030f6cb7838ef04fd
ramanandhaaja <thisisnandha@gmail.com>
2024-11-02 23:48:14 +0700

    Update InvoiceController.php

--- app/Http/Controllers/Api/V2/InvoiceController.php
+++ app/Http/Controllers/Api/V2/InvoiceController.php
@@ -16,13 +16,11 @@ class InvoiceController extends Controller
             ->paperSize(57, 200, 'mm')
             ->save('./assets/uploads/certificates/user/invoice.pdf');
 
-        /*
+        */
         return pdf()
             ->view('invoice')
             ->paperSize(57, 200, 'mm')
             ->name('invoice-2023-04-10.pdf');
-        */
 
-        return "test";
     }
 }
