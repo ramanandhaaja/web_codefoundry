4e2975ef31be1af06478af1bd4732b90b17b6b2c
ramanandhaaja <thisisnandha@gmail.com>
2024-11-03 19:28:15 +0700

    Update invoicepost.blade.php

--- resources/views/invoice/invoicepost.blade.php
+++ resources/views/invoice/invoicepost.blade.php
@@ -4,5 +4,6 @@ header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST");
 header("Allow: GET, POST");
+header('Content-Type', 'application/json');
 ?>
 {{ $jsonInvoice }}
